2e7ccd57449dc11929293d67a422ee3fc0148a1d
Denis Anisimov <denis@vaadin.com>
2016-12-08 20:29:56 +0200

    Singleton

---
+++ singleton/good/Singleton.java
@@ -0,0 +1,14 @@
+package test;
+
+public final class Singleton {
+
+    private static final Singleton INSTANCE = new Singleton();
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        return INSTANCE;
+    }
+
+}
