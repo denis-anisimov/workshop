d1f2e2aba88b695c926b34330e923df15db9ddc8
Denis Anisimov <denis@vaadin.com>
2016-12-09 20:18:14 +0200

    Api and impl in different packages for Singleton

---
+++ singleton/api/SingletonApi.java
@@ -0,0 +1,13 @@
+package test;
+
+public /* ?modifiers? */ class /* interface */ SingletonApi {
+
+    public static SingletonApi getInstance() {
+        return null;// ????
+    }
+
+    /*
+     * Некоторый набор методов, кот. определяют API но не имплементированы здесь
+     */
+
+}
